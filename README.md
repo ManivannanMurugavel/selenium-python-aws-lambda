847326b1dbebcfe8bf19efca93b51f2515b6ee8d
Manivannan Murugavel <manivannanmca2012@gmail.com>
2018-06-30 18:58:50 +0530

    completed the zip

---
+++ README.md
@@ -0,0 +1,9 @@
+# Selenium Python on AWS Lambda
+
+### No needed the Docker and your credentials
+
+
+```
+I have the final zip(lambda_function.zip) file to upload in aws lambda and you can the edit the lambda_function.py for your requirements.
+```
+
---
+++ lambda_function.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--window-size=1280x1696')
+chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+chrome_options.add_argument('--hide-scrollbars')
+chrome_options.add_argument('--enable-logging')
+chrome_options.add_argument('--log-level=0')
+chrome_options.add_argument('--v=99')
+chrome_options.add_argument('--single-process')
+chrome_options.add_argument('--data-path=/tmp/data-path')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--homedir=/tmp')
+chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+def lambda_handler(event, context):
+    # TODO implement
+    print("Starting google.com")
+    page_data = ""
+    if 'url' in event.keys():
+    	driver.get(event['url'])
+    	page_data = driver.page_source
+    	print(page_data)
+    driver.close()
+    return page_data
